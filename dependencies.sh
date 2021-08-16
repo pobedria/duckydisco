ea00888a4de66375e39d2c0bf795af2700ce45cf
Viktor Pobedria <pobedrik@gmail.com>
2021-08-16 13:15:05 +0300

    Migrated from poetry to pipenv

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-
-sudo apt install python3-dev python3-all-dev build-essential libcairo2-dev pkg-config
-sudo apt install libdbus-1-3 libdbus-1-dev
-
-# Audio libraries
-sudo apt-get install portaudio19-dev
-sudo apt-get install python-pyaudio python3-pyaudio
